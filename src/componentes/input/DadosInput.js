4c539e357726d5ebefcc31f348bd5e38e9da7fd1
robertoHenriq <newrobertsilva@gmail.com>
2025-12-02 02:08:59 -0300

    Buscar funcionando, falta apenas personalizar a exibição dos resultados

---
+++ src/componentes/input/DadosInput.js
@@ -0,0 +1,7 @@
+import Livro from '../../imagens/livro.png'
+
+export const Livros = [
+    {nome: 'Em busca do xorume perdido', id: 1, src:Livro},
+    {nome: 'JavaScript para idiotas', id: 2, src:Livro},
+    {nome: 'Em busca do xorume achado', id: 3, src:Livro},
+]
--- src/componentes/input/Index.js
+++ src/componentes/input/Index.js
@@ -1,25 +1,47 @@
 import CampoTexto from "../campoTexto"
 import Styled from "styled-components" 
 import { useState } from "react"
+import { Livros } from "./DadosInput"
 
 const InputContainer = Styled.section`
         color: #FFF;
         text-align: center;
         padding: 0px 0;
         height: 270px;
-        width: 100%;`
+        width: 100%;
+        `
+
+const LivroBuscado = Styled.div`
+    padding: 0px;
+    margin: 0px;
+    justify-content: center;
+`
+
 
 const Input = () =>{
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
+    console.log(livrosPesquisados)
+
 
     return(
         <InputContainer>
             <h2>Busque no xorume</h2>
             <CampoTexto 
                 placeholder="Buscar o xorume" 
-                onBlur={evento => setTextoDigitado(evento.target.value)}
-            />
-            <p>{textoDigitado}</p> 
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = Livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa) 
+                    }
+                }
+            /> 
+            { livrosPesquisados.map( livro =>(
+                <LivroBuscado>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src}></img>
+                </LivroBuscado>
+            )) }
         </InputContainer>
     );
 }
